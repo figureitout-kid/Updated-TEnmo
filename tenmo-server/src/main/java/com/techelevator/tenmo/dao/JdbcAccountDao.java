c10befe09f2a8f7f6e00e2ac8f03209bea7df965
Karen Reynolds <k.reynolds102@gmail.com>
2024-02-14 12:58:47 -0700

    created sql query method for retreiving balance

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,10 +1,35 @@
 package com.techelevator.tenmo.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Component
 public class JdbcAccountDao implements AccountDao {
     private final JdbcTemplate jdbcTemplate;
     public JdbcAccountDao(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+
+    @Override
+    public BigDecimal getBalance (int accountId) {
+        String sql = "SELECT balance " +
+                "FROM account " +
+                "WHERE account_id = ?;";
+
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql, accountId);
+
+        BigDecimal balance = null;
+        if(row.next())
+        {
+            balance = row.getBigDecimal("balance");
+        }
+        return balance;
+    }
+
+
+
+
+
+
 }
