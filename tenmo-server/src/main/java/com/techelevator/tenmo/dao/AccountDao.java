87dcb5721e697fac3e83626c976d9898b2d20912
Karen Reynolds <k.reynolds102@gmail.com>
2024-02-13 18:12:28 -0700

    added AccountDao interface

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -0,0 +1,13 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Account;
+
+import java.math.BigDecimal;
+
+public interface AccountDao {
+    BigDecimal getBalance(int accountId);
+    Account getAccountByUserId(int userId);
+    Account updateBalance(Account account);
+
+
+}
