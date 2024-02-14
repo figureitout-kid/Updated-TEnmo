7a221e9b884412302b99af10b24780a5e1fc028f
Karen Reynolds <k.reynolds102@gmail.com>
2024-02-13 17:26:26 -0700

    created account pojo - server side

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -0,0 +1,30 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private int accountId;
+    private int userId;
+    private BigDecimal balance;
+
+    //constructors
+    public Account(){}
+
+    public Account(int accountId, int userId, BigDecimal balance) {
+        this.accountId = accountId;
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    //getters and setters
+    public int getAccountId() { return accountId; }
+    public void setAccountId(int accountId) { this.accountId = accountId; }
+
+
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+
+    public BigDecimal getBalance() { return balance; }
+    public void setBalance(BigDecimal balance) { this.balance = balance; }
+}
