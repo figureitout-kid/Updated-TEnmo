4ce874e0a310633e9d2cf19c05efdd3d62bd919f
Karen Reynolds <k.reynolds102@gmail.com>
2024-02-17 14:09:41 -0700

    created enum TransferType for sending/receiving status codes with transfers

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferType.java
@@ -0,0 +1,33 @@
+package com.techelevator.tenmo.model;
+
+public enum TransferType {
+    REQUEST(1, "Request"),
+    SEND(2, "SEND");
+
+    private final int value;
+    private final String description;
+
+    TransferType(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static TransferType fromValue(int value) {
+        for (TransferType type : values())
+        {
+            if (type.getValue() == value)
+            {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Unknown TransferType value: " + value);
+    }
+}
