1f829dcc1c080bd899ebf34911e3c11373bbac9d
Karen Reynolds <k.reynolds102@gmail.com>
2024-02-19 14:15:04 -0700

    created jdbcTransferTypeDao class with get method

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferTypeDao.java
@@ -0,0 +1,33 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.exception.DaoException;
+import com.techelevator.tenmo.model.TransferType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+public class JdbcTransferTypeDao implements TransferTypeDao {
+    private final JdbcTemplate jdbcTemplate;
+    public JdbcTransferTypeDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public TransferType getTransferTypeById(int transferTypeId) {
+        String sql = "SELECT transfer_type_id, transfer_type_desc " +
+                     "FROM transfer_type" +
+                     "WHERE transfer_type_id = ?;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, transferTypeId);
+        if (results.next())
+        {
+            int id = results.getInt("transfer_type_id");
+            String description = results.getString("transfer_type_desc");
+
+            return TransferType.valueOf(description.toUpperCase());
+        }
+        else
+        {
+            throw new DaoException("Transfer type not found for id " + transferTypeId);
+        }
+    }
+}
